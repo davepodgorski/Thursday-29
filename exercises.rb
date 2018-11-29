30c1943519449933716c79fb1724260f32269714
davepodgorski <davepodgorski@gmail.com>
2018-11-29 16:20:18 -0500

    inish

---
+++ exercises.rb
@@ -0,0 +1,10 @@
+def word_count(str)
+
+str.split(" ").count
+
+end
+
+puts word_count("word toe something")
+puts word_count("hello world")
+puts word_count("do they have to be commas")
+puts word_count("word, toe, something")
